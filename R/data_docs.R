a462f95f010cf40e4be86c6df94bfa6f655485bc
Philip Bulsink <pbulsink@users.noreply.github.com>
2023-01-03 21:02:01 -0500

    Added more data

--- R/data_docs.R
+++ R/data_docs.R
@@ -5,6 +5,7 @@
 #' @description a data frame containing race information
 #'
 #' @format a data frame with the following columns:
+#' - raceId (numeric)
 #' - year (numeric)
 #' - round (numeric)
 #' - circuitId (corresponding to a circuitId in circuits, numeric)
@@ -54,3 +55,36 @@
 #' @source https://f1.fandom.com/wiki/Safety_Car#Safety_car_deployments_in_Formula_One_races
 #' @source fastf1 API
 "safety_cars"
+
+#' Results
+#'
+#' @description a driver-by-driver results of each race
+#'
+#' @format a data frame with the following columns:
+#' - resultId, raceId, driverId, constructorId, number, grid, position (numeric)
+#' - positionText (character)
+#' - points, laps (numeric)
+#' - time, milliseconds, fastestLap, rank, fastestLapTime, fastestLapSpeed (character)
+#' - statusId (numeric)
+#' - status (character)
+#'
+#' @source https://ergast.com/api/f1/results
+"results"
+
+#' Drivers
+#'
+#' @description a list of drivers' names, numbers, codes, and id
+#'
+#' @format a data frame with the following columns:
+#' - driverId (integer, unique)
+#' - driverRef (character, unique)
+#' - number (integer)
+#' - code (character)
+#' - forename (character)
+#' - surname (character)
+#' - dob (character, as YYYY-MM-DD)
+#' - nationality (character)
+#' - url (to wikipedia)
+#'
+#' @source https://ergast.com/api/f1/drivers
+"drivers"
