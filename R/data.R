cfe35c82071d03e875455531322cc07bde60a465
Philip Bulsink <pbulsink@users.noreply.github.com>
2023-01-04 21:11:55 -0500

    practice scraping & data - with driver data updates

--- R/data.R
+++ R/data.R
@@ -78,3 +78,171 @@ getWeather <- function(race_url){
 
   return(race_weather)
 }
+
+getPracticeTimes<-function(f1RaceId, year, practiceNum){
+  practice_results<-data.frame()
+  practice_url<-glue::glue("https://www.formula1.com/en/results.html/{year}/races/{id}/practice-{num}.html",
+                  year = year, id = f1RaceId, num = practiceNum)
+  practice_table<-NA
+
+  tryCatch({
+    practice_table<-rvest::html_table(rvest::read_html(practice_url))[[1]]
+    if("PTS" %in% colnames(practice_table)){
+      #There was no practice practiceNum, so f1 defaults back to race results
+      stop(glue::glue("There seems to be no practice {num}, {url} returns race results.",
+                      num=practiceNum, url = practice_url))
+    } else if ("Grand Prix" %in% colnames(practice_table)){
+      #There was likely an issue with the f1RaceId value
+      stop(glue::glue("There seems to be no race Id {raceid}, {url} returns season results for {year}.",
+                      raceid=f1RaceId, url = practice_url, year = year))
+    } else {
+      #hopefully all ok?
+      practice_table <- practice_table %>%
+        dplyr::select(c("Pos", "No", "Driver", "Car", "Time", "Gap", "Laps"))
+      practice_table$Driver = gsub("\\n", "", practice_table$Driver)
+      practice_table$Driver = gsub("\\s+", " ", practice_table$Driver)
+      colnames(practice_table) <- c("position", "driverNum", "driverName",
+                                    "driverCar", "time", "gap", "laps")
+
+      practice_table$driverCode<-substr(x = practice_table$driverName, nchar(practice_table$driverName)-2, nchar(practice_table$driverName))
+      practice_table$driverName<-substr(x = practice_table$driverName, 0, nchar(practice_table$driverName)-4)
+      practice_table$practiceNum <- practiceNum
+      practice_table$time <- as.character(practice_table$time)
+    }
+
+  }, error = function(e) {
+    #message(glue::glue("Error in f1model:::getPracticeTimes: {err}", err=e))
+    practice_table<-NA
+  })
+  return(practice_table)
+}
+
+getRacePractices<-function(raceId){
+  #inherently, we're not getting data for races with NA as f1RaceId
+  rs<-races[!is.na(races$f1RaceId),]
+  stopifnot(!is.na(rs[rs$raceId == raceId,]$f1RaceId))
+
+  #get practices
+  practice_data<-data.frame("position" = integer(),
+                            "driverNum" = integer(),
+                            "driverName" = character(),
+                            "driverCode" = character(),
+                            "driverCar" = character(),
+                            "time" = character(),
+                            "gap" = character(),
+                            "laps" = integer(),
+                            "practiceNum" = integer()
+  )
+
+  for(i in 1:4){
+    pd<-NA
+    pd<-try(getPracticeTimes(rs[rs$raceId == raceId,]$f1RaceId,
+                             rs[rs$raceId == raceId,]$year,i))
+
+    if(length(pd) > 1 & !("PTS" %in% colnames(pd)) & !("Q1" %in% colnames(pd)) & !("Grand Prix" %in% colnames(pd))){
+      practice_data <- dplyr::bind_rows(practice_data, pd)
+    }
+    Sys.sleep(5)
+  }
+  practice_data$raceId <- raceId
+  practice_data$f1RaceId <- rs[rs$raceId == raceId, ]$f1RaceId
+  practice_data$year <- rs[rs$raceId == raceId,]$year
+  practice_data$round <- rs[rs$raceId == raceId,]$round
+
+  #add driver/constructor lookups
+  driverConstructor <- results[results$raceId == raceId,
+                               c("driverId", "constructorId")]
+
+  practice_data$driverId <- NA
+  practice_data$constructorId <- NA
+
+  for(i in 1:nrow(practice_data)){
+    code <- practice_data[i,]$driverCode
+    if(code == "RSC"){
+      code <- "SCH"
+    } else if (code == "MOY"){
+      code <- "MON"
+    } else if (code == "CHD"){
+      code <- "CHA"
+    } else if (code == "RSI"){
+      code <- "RSS"
+    }
+    if(code == "ALB"){
+      driverId <- ifelse(practice_data[i,]$driverName == "Alexander Albon", 848, 27)
+    } else if (code == "MSC"){
+      driverId <- ifelse(practice_data[i,]$driverName == "Michael Schumacher", 30, 854)
+    } else if (code == "HAR"){
+      driverId <- ifelse(practice_data[i,]$driverName == "Brendon Hartley", 843, 837)
+    } else if (code == "BIA"){
+      driverId <- ifelse(practice_data[i,]$driverName == "Jules Bianchi", 824, 376)
+    } else if (code == "MAG"){
+      driverId <- ifelse(practice_data[i,]$driverName == "Kevin Magnussen", 825, 76)
+    } else if (code == "VER"){
+      driverId <- ifelse(practice_data[i,]$driverName == "Max Verstappen", 830, 818)
+    } else if (code == "PAN"){
+      driverId <- ifelse(practice_data[i,]$driverName == "Olivier Panis", 44, 45)
+    } else {
+      driverId <- drivers[drivers$code == code,]$driverId
+    }
+
+    if(length(driverId) != 0){
+      practice_data[i, ]$driverId <- driverId
+      constructorId <- driverConstructor[driverConstructor$driverId == driverId,]$constructorId
+      if (length(constructorId) > 0){
+        practice_data[i, ]$constructorId <- constructorId
+      }
+    } else {
+      message(glue::glue("Driver {driver} not found: {drivername}",
+                         driver = code,
+                         drivername = practice_data[i,]$driverName))
+      next
+    }
+    if(length(constructorId) == 0) {
+      const<-practice_data[i, ]$driverCar
+      practice_data[i, ]$constructorId <- dplyr::case_when(
+        grepl("^ferrari", const, ignore.case = T) ~ 6,
+        grepl("^mclaren", const, ignore.case = T) ~ 1,
+        grepl("BAR", const, fixed = T, ignore.case = F) ~ 16,
+        const == "Sauber BMW" ~ 2,
+        grepl("^sauber", const, ignore.case = T) ~ 15,
+        grepl("jordan", const, ignore.case = T) ~ 17,
+        grepl("benetton", const, ignore.case = T) ~ 22,
+        grepl("jaguar", const, ignore.case = T) ~ 19,
+        grepl("minardi", const, ignore.case = T) ~ 18,
+        grepl("arrows", const, ignore.case = T) ~ 21,
+        grepl("^williams", const, ignore.case = T) ~ 3,
+        grepl("prost", const, ignore.case = T) ~ 20,
+        grepl("^renault", const, ignore.case = T) ~ 4,
+        grepl("^red bull", const, ignore.case = T) ~ 9,
+        grepl("rbr", const, ignore.case = T) ~ 9,
+        grepl("haas", const, ignore.case = T) ~ 210,
+        grepl("^alfa romeo", const, ignore.case = T) ~ 51,
+        grepl("alphatauri", const, ignore.case = T) ~ 213,
+        grepl("alpine", const, ignore.case = T) ~ 214,
+        grepl("^mercedes", const, ignore.case = T) ~ 131,
+        grepl("^aston martin", const, ignore.case = T) ~ 117,
+        grepl("torro rosso", const, ignore.case = T) ~ 5,
+        grepl("toro rosso", const, ignore.case = T) ~ 5,
+        grepl("stryker", const, ignore.case = T) ~ 14,
+        grepl("^str ", const, ignore.case = T) ~ 5,
+        grepl("force india", const, ignore.case = T) ~ 10,
+        grepl("mf1", const, ignore.case = T) ~ 13,
+        grepl("^honda", const, ignore.case = T) ~ 11,
+        grepl("^toyota", const, ignore.case = T) ~ 7,
+        grepl("aguri", const, ignore.case = T) ~ 8,
+        grepl("^hrt", const, ignore.case = T) ~ 164,
+        grepl("^virgin", const, ignore.case = T) ~ 166,
+        grepl("caterham", const, ignore.case = T) ~ 207,
+        grepl("marussia", const, ignore.case = T) ~ 206,
+        grepl("lotus mercedes", const, ignore.case = T) ~ 208,
+        TRUE ~ NA_real_)
+      if(is.na(practice_data[i, ]$constructorId)){
+        message(glue::glue("Found Unknown driverCar: {car} for Driver {driver} in raceId: {race}",
+                         car = const, driver = practice_data[i,]$driverCode,
+                         race = raceId))
+      }
+    }
+  }
+
+  return(practice_data)
+}
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,7 +1,20 @@
 test_that("Weather works", {
   expect_error(getWeather("bob.com"))
-  expect_equal(getWeather("https://en.wikipedia.org/wiki/2021_Austrian_Grand_Prix"), 'cloudy')
-  expect_equal(getWeather("https://en.wikipedia.org/wiki/2021_British_Grand_Prix"), 'warm')
+  expect_equal(getWeather("https://en.wikipedia.org/wiki/2021_Austrian_Grand_Prix"),
+               'cloudy')
+  expect_equal(getWeather("https://en.wikipedia.org/wiki/2021_British_Grand_Prix"),
+               'warm')
   #The Russian Grand Prix is listed as 'Cloudy and rainy', 'wet' should beat out 'cloudy'
-  expect_equal(getWeather("https://en.wikipedia.org/wiki/2021_Russian_Grand_Prix"), 'wet')
+  expect_equal(getWeather("https://en.wikipedia.org/wiki/2021_Russian_Grand_Prix"),
+               'wet')
+})
+
+test_that("Practice Download Works", {
+  practice<-getRacePractices(969)
+  expect_equal(practice[practice$driverCode == "WEH","constructorId"][1], 15)
+  expect_equal(practice[practice$driverCode == "RAI", "driverId"][1], 8)
+  expect_equal(colnames(practice), c("position", "driverNum", "driverName",
+                                     "driverCode", "driverCar", "time", "gap",
+                                     "laps", "practiceNum", "raceId", "f1RaceId",
+                                     "year", "round", "driverId", "constructorId"))
 })
