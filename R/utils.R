0097bd22088849f673660063ba2b8ba85f31f26b
pbulsink <pbulsink@users.noreply.github.com>
2023-01-09 01:01:04 +0000

    Style code

--- R/utils.R
+++ R/utils.R
@@ -4,10 +4,10 @@ getCurrentSeason <- function() {
 
 .timeToSec <- function(t) {
   if (grepl("+", t, fixed = T)) {
-    t<-gsub("+", "", t, fixed = T)
+    t <- gsub("+", "", t, fixed = T)
   }
   if (grepl("s", t, fixed = T)) {
-    t<-gsub("s", "", t, fixed = T)
+    t <- gsub("s", "", t, fixed = T)
   }
   ms <- as.integer(strsplit(t, ".", fixed = T)[[1]][2])
   minsec <- strsplit(t, ".", fixed = T)[[1]][1]
@@ -25,21 +25,21 @@ getCurrentSeason <- function() {
 timeToSec <- Vectorize(.timeToSec, SIMPLIFY = T, USE.NAMES = F)
 
 ewma <- function(x, a) {
-  x<-as.numeric(x)
-  if(min(which(!is.na(x))) != 1){
-    #handle if a vector starts with NA values
-    nastart<-min(which(!is.na(x)))
-    x<-x[nastart:length(x)]
+  x <- as.numeric(x)
+  if (min(which(!is.na(x))) != 1) {
+    # handle if a vector starts with NA values
+    nastart <- min(which(!is.na(x)))
+    x <- x[nastart:length(x)]
   } else {
-    nastart<-0
+    nastart <- 0
   }
   # From https://stackoverflow.com/q/42774001 but they had alpha backwards
   s1 <- x[1]
   sk <- s1
-  s <- vapply(x[-1], function(x) ifelse(is.na(x), sk, sk <<- a * x + (1-a) * sk), 0)
+  s <- vapply(x[-1], function(x) ifelse(is.na(x), sk, sk <<- a * x + (1 - a) * sk), 0)
   s <- c(s1, s)
-  if(nastart){
-    s<-c(rep(NA, nastart-1), s)
+  if (nastart) {
+    s <- c(rep(NA, nastart - 1), s)
   }
   return(s)
 }
