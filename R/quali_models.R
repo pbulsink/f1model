cd0bb57270c1729a7ee60c2c7b6c59feba51b61b
Philip Bulsink <bulsinkp@gmail.com>
2023-02-09 21:14:06 -0500

    Added first stacks code.

--- R/quali_models.R
+++ R/quali_models.R
@@ -211,7 +211,7 @@ buildQualiModel_svm <- function(model_data = combineData()) {
     recipes::step_string2factor(c("circuitType", "circuitDirection")) %>%
     recipes::step_dummy(recipes::all_nominal_predictors()) %>%
     recipes::step_normalize(recipes::all_numeric()) %>%
-    recipes::step_zv(recipes::all_predictors())
+    recipes::step_nzv(recipes::all_predictors())
 
   tune_wf <- workflows::workflow() %>%
     workflows::add_recipe(init_recipe) %>%
@@ -357,7 +357,7 @@ buildQualiModel_xgb <- function(model_data = combineData()) {
                               roc_train = round(train_roc, 4), roc_test = round(test_roc, 4)
   ))
   return(final_model)
-} # SLOW
+} # SLOW. preprocess ok.
 
 buildQualiModel_knn <- function(model_data = combineData()) {
   # Thin Data
@@ -724,6 +724,167 @@ buildQualiModel_CART <- function(model_data = combineData()) {
   return(final_model)
 } # 2 minutes, preprocess ok ~0.73
 
+
 buildQualiModel_mix <- function(model_data = combineData()) {
-  NULL
+  # ----- Thin Data -----
+  model_data <- model_data %>%
+    dplyr::select(c(
+      # ID columns
+      "raceId", "circuitId", "driverId", "currentConstructorId",
+      # quali data (TARGET)
+      "qPosition",
+      # driver data
+      "driverDOB", "driverAge", "driverGPExperience", "driverSeasonWins",
+      "driverSeasonWinsPerc", "driverPoints", "driverPointsPerc", "driverHomeRace",
+      # constructor data
+      "constructorPoints", "constructorSeasonWins", "constructorPointsPerc",
+      "constructorSeasonWinsPerc", "constructorHomeRace",
+      # circuit data
+      "circuitAltitude", "circuitLength", "circuitType", "circuitDirection",
+      # practice data
+      "driverPercPracticeLaps", "constructorPercPracticeLaps", "driverPracticeBestPerc", "driverPracticeAvgPerc",
+      "constructorPracticeBestPerc", "constructorPracticeAvgPerc", "driverTeamPracticeAvgGapPerc"
+    )) %>%
+    dplyr::filter(.data$qPosition > 0) %>%
+    dplyr::filter(.data$qPosition <= 20) %>%
+    dplyr::mutate("qPosition" = as.character(.data$qPosition))
+
+  # Split data
+  splitdata <- rsample::initial_split(model_data, 0.8)
+  training_data <- rsample::training(splitdata)
+  test_data <- rsample::testing(splitdata)
+
+  folds <- rsample::vfold_cv(training_data)
+
+  # ----- General setup -----
+  model_recipe <- recipes::recipe(qPosition ~ ., training_data) %>%
+    recipes::update_role("raceId", new_role = "ID") %>%
+    recipes::update_role("circuitId", new_role = "ID") %>%
+    recipes::update_role("driverId", new_role = "ID") %>%
+    recipes::update_role("currentConstructorId", new_role = "ID") %>%
+    recipes::step_string2factor(c("qPosition", "circuitType", "circuitDirection")) %>%
+    recipes::step_nzv(recipes::all_predictors())
+
+  model_workflow <- workflows::workflow() %>%
+    workflows::add_recipe(model_recipe)
+
+  ctrl_grid <- stacks::control_stack_grid()
+
+  # ----- Decision Tree -----
+  decision_tree_spec <- parsnip::decision_tree(
+    cost_complexity = tune::tune(),
+    tree_depth = tune::tune()
+  ) %>%
+    parsnip::set_mode("classification") %>%
+    parsnip::set_engine("rpart")
+
+  decision_tree_wflow <-
+    model_workflow %>%
+    workflows::add_model(decision_tree_spec)
+
+  decision_tree_res <-
+    tune::tune_grid(
+      object = decision_tree_wflow,
+      resamples = folds,
+      grid = 10,
+      control = ctrl_grid
+    )
+
+  # ----- Naive Bayes -----
+  # naive_bayes_spec <- parsnip::naive_Bayes(
+  #   smoothness = tune::tune(),
+  #   Laplace = tune::tune()
+  # ) %>%
+  #   parsnip::set_mode("classification") %>%
+  #   parsnip::set_engine("klaR")
+  #
+  # naive_bayes_wflow <-
+  #   model_workflow %>%
+  #   add_model(naive_bayes_spec)
+  #
+  # naive_bayes_res <-
+  #   tune::tune_grid(
+  #     object = naive_bayes_wflow,
+  #     resamples = folds,
+  #     grid = 10,
+  #     control = ctrl_grid
+  #   )
+
+  # ----- SVM Linear - s2f, dummy, norm, nzv-----
+
+  # ----- KNN -----
+  knn_spec <- parsnip::nearest_neighbor(
+    neighbors = tune::tune()
+    ) %>%
+    parsnip::set_mode("classification") %>%
+    parsnip::set_engine("kknn")
+
+  knn_rec <-
+    model_recipe %>%
+    recipes::step_normalize(recipes::all_numeric_predictors()) %>%
+    recipes::step_nzv(recipes::all_predictors())
+
+  knn_workflow <-
+    model_workflow %>%
+    workflows::add_model(knn_spec) %>%
+    workflows::update_recipe(knn_rec)
+
+  knn_res <- tune::tune_grid(
+    object = knn_workflow,
+    resamples = folds,
+    grid = 10,
+    control = ctrl_grid
+  )
+
+  # ----- GLM - s2f, dummy, nzv  -----
+  glm_spec <- parsnip::multinom_reg(
+    penalty = tune::tune(),
+    mixture = tune::tune()
+  ) %>%
+    parsnip::set_mode("classification") %>%
+    parsnip::set_engine("glmnet")
+
+  glm_rec <-
+    model_recipe %>%
+    recipes::step_dummy(recipes::all_nominal_predictors()) %>%
+    recipes::step_nzv(recipes::all_predictors())
+
+  glm_workflow <-
+    model_workflow %>%
+    workflows::add_model(glm_spec) %>%
+    workflows::update_recipe(glm_rec)
+
+  glm_res <- tune::tune_grid(
+    object = glm_workflow,
+    resamples = folds,
+    grid = 10,
+    control = ctrl_grid
+  )
+  # ----- CART - s2f, dummy, nzv  -----
+
+  # ----- STACK IT -----
+  model_stack <-
+    # initialize the stack
+    stacks::stacks() %>%
+    # add candidate members
+    stacks::add_candidates(decision_tree_res) %>%
+    #stacks::add_candidates(naive_bayes_res) %>%
+    stacks::add_candidates(knn_res) %>%
+    stacks::add_candidates(glm_res) %>%
+    # determine how to combine their predictions
+    stacks::blend_predictions() %>%
+    # fit the candidates with nonzero stacking coefficients
+    stacks::fit_members()
+
+  model_pred <-
+    test_data %>%
+    bind_cols(predict(model_stack, ., type = "prob"))
+
+  yardstick::roc_auc(
+    model_pred,
+    truth = qPosition,
+    contains(".pred_")
+  )
+
+  return(model_stack)
 }
